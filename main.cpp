e3473bf709cac1d8c485168e6c09ff78afff7cd9
Yash Agrawal <38642926+ingenuo-yag@users.noreply.github.com>
2019-10-08 15:39:29 +0530

    Hello World in cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
-/****************************************************
-*   Template for coding contests                    *
-*   Author    :    Sanjeev Sharma                   *
-*   Email     :    thedevelopersanjeev@gmail.com    *
-*****************************************************/
- 
-#include <bits/stdc++.h>
+//
+//  main.cpp
+//  Hello-World
+//
+//  Created by ingenuo-yag on 09/07/19.
+//  Copyright © 2019 ingenuo-yag. All rights reserved.
+//
+
+#include <iostream>
+#include<string>
 using namespace std;
 
-#define deb(x) cout <<#x <<" " <<x <<endl;
-
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
+int main()
+{
+    //cout<<"This is the test file\n";
+    //int a = 5;//copy initialisation
+    //int b(6);//direct initialisation
+    //int c{5};//uniform initialisation(preferred)
+    //int n;
+    //Initialization gives a variable a value at the point when it is created. Assignment gives a variable a value at some point after it is created.
+    //cout<<a<<b<<c<<endl;
+    //cout<<n<<endl;
+    //'/n' is preferred over endl
+    string a = "abc";
+    int n = (int)a.size();
+    cout<<n<<endl;
     return 0;
 }
