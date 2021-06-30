712ffd7bd550c2297e5d239349e5b3ae473d2ecc
codersanjeev <thedevelopersanjeev@gmail.com>
2021-06-30 20:40:55 +0530

    added new questions

--- LeetCode/Lowest Common Ancestor of a Binary Search Tree/main.kt
+++ LeetCode/Lowest Common Ancestor of a Binary Search Tree/main.kt
@@ -8,6 +8,19 @@
 
 class Solution {
     fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
-        
+        if (root == null || root == p || root == q) {
+            return root
+        }
+        var left = lowestCommonAncestor(root.left, p, q)
+        var right = lowestCommonAncestor(root.right, p, q)
+        if (left == null && right == null) {
+            return null
+        } else if (left != null && right != null) {
+            return root
+        } else if (left == null) {
+            return right
+        } else {
+            return left
+        }
     }
 }
--- LeetCode/Lowest Common Ancestor of a Binary Search Tree/main.swift
+++ LeetCode/Lowest Common Ancestor of a Binary Search Tree/main.swift
@@ -13,45 +13,16 @@
  */
 
 class Solution {
-
-    var level = [TreeNode : Int]()
-    var parent = [TreeNode : TreeNode]()
-
-    private func dfs(_ root: TreeNode?) {
-        guard let root = root else { return }
-        if let left = root.left {
-            parent[left] = root
-            level[left] = (level[root] ?? 0) + 1
-            dfs(left)
-        }
-        if let right = root.right {
-            parent[right] = root
-            level[right] = (level[root] ?? 0) + 1
-            dfs(right)
-        }
-    }
-
     func lowestCommonAncestor(_ root: TreeNode?, _ a: TreeNode?, _ b: TreeNode?) -> TreeNode? {
-        parent[root] = root
-        level[root] = 0
-        dfs(root)
-        var p = a
-        var q = b
-        if (level[p] ?? 0) < (level[q] ?? 0) {
-            var temp = p
-            p = q
-            q = temp
+        if let root = root {
+            if root.val == a?.val || root.val == b?.val { return root }
+            var left = lowestCommonAncestor(root.left, a, b)
+            var right = lowestCommonAncestor(root.right, a, b)
+            if left == nil && right == nil { return nil }
+            if left != nil && right != nil { return root }
+            return left == nil ? right : left
+        } else {
+            return nil
         }
-        var d = (level[p] ?? 0) - (level[q] ?? 0)
-        while d > 0 {
-            p = parent[p]
-            d -= 1
-        }
-        if p == q { return p }
-        while parent[p] != parent[q] {
-            p = parent[p]
-            q = parent[q]
-        }
-        return parent[p]
     }
 }
